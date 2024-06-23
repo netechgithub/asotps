f6bada51c7be3f47c640958655eae9b6fd89bb91
netech <nahuel.barrueto@gmail.com>
2024-06-23 20:02:02 -0300

    aplicarcambios

--- TP5/ciudad.sh
+++ TP5/ciudad.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# Clave de API de weatherapi.com (reemplaza con tu propia clave)
+# Clave de API
 apiKEY="8719255a66df4f5792d202736242406"
 
-# Solicitar al usuario que ingrese una ciudad
+# Para que el usuario ingrese la ciudad
 read -p "Ingrese una ciudad para saber su clima actual: " ciudad_ini
 
-# Reemplazar espacios en la ciudad por %20 para la URL
 ciudad_ingresada=$(echo "$ciudad_ini" | tr ' ' '%20')
 
-# Construir la URL de la API
+# URL de la API
 url="http://api.weatherapi.com/v1/current.json?key=$apiKEY&q=$ciudad_ingresada&aqi=no"
 
-# Hacer la solicitud GET a la API y guardar la respuesta en un archivo JSON
 curl --silent --request GET "$url" --output respuesta.json
 
-# Verificar si se obtuvo la respuesta correctamente y no está vacía
 if [ -s respuesta.json ]; then
     # Comprobar si la respuesta contiene un error
     error=$(jq -r '.error.message' respuesta.json)
