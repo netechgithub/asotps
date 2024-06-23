e38b8a3f6b0c6d69dc89dbcfb449c87cad7b5cc0
netech <nahuel.barrueto@gmail.com>
2024-06-23 20:06:56 -0300

    ciudad

--- TP5/ciudad.sh
+++ TP5/ciudad.sh
@@ -3,7 +3,14 @@
 # Clave de API
 apiKEY="8719255a66df4f5792d202736242406"
 
-# Para que el usuario ingrese la ciudad
+# Función para imprimir en color
+print_color() {
+    color_code=$1
+    message=$2
+    echo -e "\e[${color_code}m${message}\e[0m"
+}
+
+# Solicitar al usuario que ingrese la ciudad
 read -p "Ingrese una ciudad para saber su clima actual: " ciudad_ini
 
 ciudad_ingresada=$(echo "$ciudad_ini" | tr ' ' '%20')
@@ -11,23 +18,25 @@ ciudad_ingresada=$(echo "$ciudad_ini" | tr ' ' '%20')
 # URL de la API
 url="http://api.weatherapi.com/v1/current.json?key=$apiKEY&q=$ciudad_ingresada&aqi=no"
 
+# Hacer la solicitud GET a la API y guardar la respuesta en un archivo JSON
 curl --silent --request GET "$url" --output respuesta.json
 
 if [ -s respuesta.json ]; then
     # Comprobar si la respuesta contiene un error
     error=$(jq -r '.error.message' respuesta.json)
     if [ "$error" != "null" ]; then
-        echo "Error: $error"
+        print_color "31" "Error: $error"
     else
         # Conseguir la temperatura actual del JSON usando jq
         clima=$(jq -r '.current.temp_c' respuesta.json)
         condicion=$(jq -r '.current.condition.text' respuesta.json)
 
         # Mostrar el clima actual de la ciudad
-        echo "El clima actual de $ciudad_ini es: $clima°C y el estado es: $condicion"
+        print_color "32" "El clima actual de $ciudad_ini es: $clima°C"
+        print_color "34" "Estado: $condicion"
     fi
 else
-    echo "No se pudo obtener el clima para la ciudad: $ciudad_ini."
+    print_color "31" "No se pudo obtener el clima para la ciudad: $ciudad_ini."
 fi
 
 # Eliminar el archivo temporal
